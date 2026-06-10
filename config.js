18028111c2ea4c148fc611b0e0d292a92120ada7
MyTuteEliz <mytutor.elizabeth@gmail.com>
2026-06-09 19:56:38 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+/*
+====================================================
+FILE: config.js
+
+PURPOSE:
+Global behavior settings
+
+SAFE TO EDIT:
+- progression mode
+====================================================
+*/
+
+export const CONFIG = {
+  progressionMode: "guided-flexible",
+  dictationMode: "audio-first"
+};
