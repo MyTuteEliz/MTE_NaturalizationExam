8314019758d58bddaad098d7bb05f36239d92fcf
MyTuteEliz <mytutor.elizabeth@gmail.com>
2026-06-09 19:56:00 -0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,17 @@
+/*
+====================================================
+FILE: auth.js
+
+PURPOSE:
+Password gate system (versioned)
+
+EDIT ONLY:
+- accessCode
+- appVersion
+====================================================
+*/
+
+export const AUTH = {
+  accessCode: "1234",
+  appVersion: "1.0"
+};
